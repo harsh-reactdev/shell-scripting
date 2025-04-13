834abf097bf4abd402e75da1270ebb92ae254ed2
harsh <harsh.reactdev@gmail.com>
2025-04-14 00:26:21 +0530

    chore: reading variables from user

--- hello.sh
+++ hello.sh
@@ -10,6 +10,31 @@ echo $HOME
 echo $PWD
 
 # user defined variables
-name = harsh
+name=harsh
 
 echo My name is $name
+
+# taking input from the user
+
+echo Enter your age : 
+read age
+echo Your age is : $age
+
+echo Enter your name and age :
+read name age # reading multiple user inputs
+echo My name is $name and my age is $age
+
+# printing msg, reading inputs and saving into variables all in the same line
+read -p 'Enter your username :' username
+
+# keeping the user entered text hidden while entering
+read -sp 'Enter your password :' password
+echo
+
+echo $username $password
+
+# reading multiple inputs and saving it in an array
+echo What are your interests ? 
+read -a interests
+# echo ${interests[0]} ${interests[1]} ${interests[2]} ${interests[3]}
+echo -a $interests
