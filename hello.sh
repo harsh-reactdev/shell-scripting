4387032a2de906981d2a3cc6c29bc20612b5eb24
harsh <harsh.reactdev@gmail.com>
2025-04-13 18:28:53 +0530

    chore: comments and variables

---
+++ hello.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# this is a comment
+echo "Hello world.!" # this is a comment as well
+
+# system variables
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
+
+# user defined variables
+name = harsh
+
+echo My name is $name
