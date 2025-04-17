befec45d0afdb5f4a96d0f2365a0a90fe87484fe
harsh <harsh.reactdev@gmail.com>
2025-04-18 00:22:55 +0530

    chore : day4 complete

--- day4.sh
+++ day4.sh
@@ -11,6 +11,7 @@ args=("$@")
 echo $args # only prints the first argument in the argument list
 
 echo ${args[0]} ${args[1]} ${args[2]} ${args[3]}
-# echo ${args[3]}
-# null because assigning passed arguments to array starts at 0 and not at 1
+# echo ${args[3]} --> null because assigning passed arguments to array starts at 0 and not at 1
 # so when i run ./day4.sh harsh is dev, harsh is assigned to args[0], is -> args[1] and dev is assigned to args[2] and args[3] is undefined
+
+echo ${args[@]} # prints all the arguments passed to the script
